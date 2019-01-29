3af2fb25608a62d51248e1c6fce18750afb3e17c
Manuj Kathuria <kathuria.manuj@gmail.com>
2019-01-30 03:59:21 +0700

    Broadcast Input Stream

--- src/main/scala/basics/BroadcastStreams.scala
+++ src/main/scala/basics/BroadcastStreams.scala
@@ -11,16 +11,16 @@ object BroadcastStreams extends App {
   val graph = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
     import GraphDSL.Implicits._
 
-    val source = Source(1 to 5)
+    val source = Source(1 to 1000000)
     val multiplierTwo = Flow[Int].map(_ * 2)
     val multiplierThree = Flow[Int].map(_ * 3)
     val sink = Sink.foreach[Int](s => println(s"[output] => $s"))
 
-    val bcast = b.add(Broadcast[Int](2))
-    source ~> bcast.in
+    val broadcast = b.add(Broadcast[Int](2))
+    source ~> broadcast.in
 
-    bcast.out(0) ~> multiplierTwo ~> sink
-    bcast.out(1) ~> multiplierThree ~> sink
+    broadcast.out(0) ~> multiplierTwo ~> sink
+    broadcast.out(1) ~> multiplierThree ~> sink
 
     ClosedShape
   })
